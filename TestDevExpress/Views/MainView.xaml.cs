34aba2246b4f1fd8fa999eaa34fc3607cfa33719
DESKTOP-LH420JD\Ivanc <ivancurs10091984@gmail.com>
2021-08-10 14:45:06 +0300

    H

--- TestDevExpress/Views/MainView.xaml.cs
+++ TestDevExpress/Views/MainView.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using DevExpress.Xpf.Bars;
+﻿using DevExpress.Xpf.Bars;
 using Microsoft.Win32;
+using System;
 using System.Linq;
-using System.Windows.Controls;
-using DevExpress.Xpf.Printing;
 using System.Windows;
 
 namespace TestDevExpress.Views
@@ -11,7 +9,7 @@ namespace TestDevExpress.Views
     /// <summary>
     /// Interaction logic for View1.xaml
     /// </summary>
-    public partial class MainView : UserControl
+    public partial class MainView
     {
         public MainView()
         {
